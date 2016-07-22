014ddeed91c5e4114a665526f061bd80b5ccaa4d
Christian Roese <croese@gmail.com>
2016-07-21 20:06:44 -0500

    Adds saving/loading and UI from chapter 5

--- Notes/Document.swift
+++ Notes/Document.swift
@@ -8,36 +8,91 @@
 
 import Cocoa
 
+/// Names of files/directories in the package
+enum NoteDocumentFileNames : String {
+  case TextFile = "Text.rtf"
+  case AttachmentsDirectory = "Attachments"
+}
+
+enum ErrorCode : Int {
+  /// We couldn't find the document at all
+  case CannotAccessDocument
+  
+  /// We couldn't access any file wrappers inside this document
+  case CannotLoadFileWrappers
+  
+  /// We couldn't load the Text.rtf file
+  case CannotLoadText
+  
+  /// We couldn't access the Attachments folder
+  case CannotAccessAttachments
+  
+  /// We couldn't save the Text.rtf file
+  case CannotSaveText
+  
+  /// We couldn't save an attachment
+  case CannotSaveAttachment
+}
+
+let ErrorDomain = "NotesErrorDomain"
+
+func err(code: ErrorCode, _ userInfo: [NSObject:AnyObject]? = nil) -> NSError {
+  return NSError(domain: ErrorDomain, code: code.rawValue, userInfo: userInfo)
+}
+
 class Document: NSDocument {
-
+  
+  var text: NSAttributedString = NSAttributedString()
+  var documentFileWrapper = NSFileWrapper(directoryWithFileWrappers: [:])
+  
   override init() {
-      super.init()
+    super.init()
     // Add your subclass-specific initialization here.
+    
   }
-
+  
   override class func autosavesInPlace() -> Bool {
     return true
   }
-
+  
   override var windowNibName: String? {
     // Returns the nib file name of the document
     // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
     return "Document"
   }
-
-  override func dataOfType(typeName: String) throws -> NSData {
-    // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-    // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+  
+  override func fileWrapperOfType(typeName: String) throws -> NSFileWrapper {
+    let textRTFData = try self.text.dataFromRange(NSRange(0..<self.text.length), documentAttributes: [
+      NSDocumentTypeDocumentAttribute : NSRTFTextDocumentType
+      ])
+    
+    // replace existing text file, if it exists in the wrapper
+    if let oldTextFileWrapper = self.documentFileWrapper.fileWrappers?[NoteDocumentFileNames.TextFile.rawValue] {
+      self.documentFileWrapper.removeFileWrapper(oldTextFileWrapper)
+    }
+    
+    self.documentFileWrapper.addRegularFileWithContents(textRTFData, preferredFilename: NoteDocumentFileNames.TextFile.rawValue)
+    
+    return self.documentFileWrapper
   }
-
-  override func readFromData(data: NSData, ofType typeName: String) throws {
-    // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-    // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-    // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-    throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+  
+  override func readFromFileWrapper(fileWrapper: NSFileWrapper, ofType typeName: String) throws {
+    guard let fileWrappers = fileWrapper.fileWrappers else {
+      throw err(.CannotLoadFileWrappers)
+    }
+    
+    guard let documentTextData = fileWrappers[NoteDocumentFileNames.TextFile.rawValue]?.regularFileContents else {
+      throw err(.CannotLoadText)
+    }
+    
+    guard let documentText = NSAttributedString(RTF: documentTextData, documentAttributes: nil) else {
+      throw err(.CannotLoadText)
+    }
+    
+    self.documentFileWrapper = fileWrapper
+    
+    self.text = documentText
   }
-
-
+  
 }
 
